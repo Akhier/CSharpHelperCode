4511ce38faf344d77ec737579756bb762d2e48f6
Akhier <akhierthedragonhearted@yahoo.com>
2014-02-10 17:36:39 -0500

    +tests for PrioityQueue
    addThenRemoveOneInt()
    addThenRemoveOneNegativeInt()
    addThenRemoveOutOfOrderInts()
    addMultipleRemoveOneInt()
    noValuesSet()

---
+++ CSharpHelperTest/testPriorityQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpHelperCode;
+
+namespace CSharpHelperTest
+{
+    [TestClass]
+    public class testPriorityQueue
+    {
+        [TestMethod]
+        public void addThenRemoveOneInt() {
+            PriorityQueue<int> testQueue = new PriorityQueue<int>();
+            testQueue.Enqueue(1);
+            Assert.AreEqual(1, testQueue.Dequeue());
+        }
+
+        [TestMethod]
+        public void addThenRemoveOneNegativeInt() {
+            PriorityQueue<int> testQueue = new PriorityQueue<int>();
+            testQueue.Enqueue(-1);
+            Assert.AreEqual(-1, testQueue.Dequeue());
+        }
+
+        [TestMethod]
+        public void addThenRemoveOutOfOrderInts() {
+            PriorityQueue<int> testQueue = new PriorityQueue<int>();
+            testQueue.Enqueue(2);
+            testQueue.Enqueue(1);
+            Assert.IsTrue(testQueue.Dequeue() < testQueue.Dequeue());
+        }
+
+        [TestMethod]
+        public void addMultipleRemoveOneInt() {
+            PriorityQueue<int> testQueue = new PriorityQueue<int>();
+            testQueue.Enqueue(5);
+            testQueue.Enqueue(10);
+            testQueue.Enqueue(-2);
+            testQueue.Enqueue(1);
+            testQueue.Enqueue(2);
+            testQueue.Enqueue(0);
+            Assert.AreEqual(-2, testQueue.Dequeue());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void noValuesSet() {
+            PriorityQueue<int> testQueue = new PriorityQueue<int>();
+            int i = testQueue.Dequeue();
+        }
+    }
+}
