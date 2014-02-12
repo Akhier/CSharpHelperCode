3aa5298dcfa49c213620eb4274f83446222a2ce6
Akhier <akhierthedragonhearted@yahoo.com>
2014-02-12 13:59:48 -0500

    +Graph variables added

---
+++ CSharpDijkstraAlgorithm/C_Graph.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDijkstraAlgorithm
+{
+    class Graph {
+        private Vector2D _sourceNode;
+        private List<Vector2D> _listOfNodes;
+        private List<Edge> _listOfEdges;
+        public List<Vector2D> AllNodes {
+            get { return _listOfNodes; }
+        }
+        public Vector2D SourceVector {
+            get { return _sourceNode; }
+            set {
+                for (int i = 0; i < _listOfNodes.Count; i++) {
+                    if (_listOfNodes[i] == value) {
+                        _sourceNode = value;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
