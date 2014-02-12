998892768dced208a11caca50291bcc2b6846762
Akhier <akhierthedragonhearted@yahoo.com>
2014-02-12 13:47:36 -0500

    *Edge fully implemented

--- CSharpDijkstraAlgorithm/C_Edge.cs
+++ CSharpDijkstraAlgorithm/C_Edge.cs
@@ -7,8 +7,18 @@ using System.Threading.Tasks;
 namespace CSharpDijkstraAlgorithm
 {
     class Edge {
-        public int Cost, EdgeID;
-        public static int EdgeIDCount = 0;
-        public Vector2D PointA, PointB;
+        public int Cost;
+        private int _edgeID;
+        private static int _edgeIDCount = 0;
+        private Vector2D _pointA, _pointB;
+        public int EdgeID {
+            get { return _edgeID; }
+        }
+        public Vector2D PointA {
+            get { return _pointA; }
+        }
+        public Vector2D PointB {
+            get { return _pointB; }
+        }
     }
 }
