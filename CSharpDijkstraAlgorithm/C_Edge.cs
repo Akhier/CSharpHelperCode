df0a599d9d217d76379916db647f00eba393d26c
Akhier <akhierthedragonhearted@yahoo.com>
2014-02-12 13:51:08 -0500

    *Edge actually fully implemented

--- CSharpDijkstraAlgorithm/C_Edge.cs
+++ CSharpDijkstraAlgorithm/C_Edge.cs
@@ -20,5 +20,25 @@ namespace CSharpDijkstraAlgorithm
         public Vector2D PointB {
             get { return _pointB; }
         }
+        public Edge(Vector2D pointA, Vector2D pointB, int cost) {
+            Cost = cost;
+            _pointA = pointA;
+            _pointB = pointB;
+            _edgeID = ++_edgeIDCount;
+        }
+        public Vector2D getOtherVector(Vector2D baseVector) {
+            if (baseVector == _pointA) {
+                return _pointB;
+            }
+            else if (baseVector == _pointB) {
+                return _pointA;
+            }
+            else {
+                return null;   //if you manage to have a vector which isn't A or B
+            }
+        }
+        public override string ToString() {
+            return "Edge ID: " + _edgeID + " - Connected to vectors " + _pointA.VectorID + " and " + _pointB.VectorID + " at a cost of " + Cost;
+        }
     }
 }
